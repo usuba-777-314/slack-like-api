c9262fabd0889dc8ba40954a038cc41c2dbfccfe
Igawa Hironobu <zin920314@gmail.com>
2020-05-30 12:58:35 +0900

    feat: メッセージ送信APIを作成

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,14 @@
+require 'date'
+
 class Api::MessagesController < ApplicationController
   def index
     @messages = Message.all
+  end
+
+  def create
+    data = params.require(:message).permit(:text)
+    data[:timestamp] = DateTime.now
+
+    @message = Message.create data
   end
 end
---
+++ app/views/api/messages/create.json.jbuilder
@@ -0,0 +1,4 @@
+json.message do
+  json.text @message.text
+  json.timestamp @message.timestamp
+end
